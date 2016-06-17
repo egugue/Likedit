c0c6f0156f75d88ccc14b1c81cd7b8f99d355fef
Haruyoshi Toyama <duane0728@gmail.com>
2016-06-18 01:34:41 +0900

    add rendering tweet (yet wip)

--- app/src/main/kotlin/com/htoyama/likit/ui/TweetAdapter.kt
+++ app/src/main/kotlin/com/htoyama/likit/ui/TweetAdapter.kt
@@ -1,8 +1,10 @@
 package com.htoyama.likit.ui
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.htoyama.likit.R
 
 import com.twitter.sdk.android.core.models.Tweet
 import com.twitter.sdk.android.tweetui.BaseTweetView
@@ -14,7 +16,7 @@ import java.util.ArrayList
  * Created by toyamaosamuyu on 2016/06/17.
  */
 class TweetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-  private var tweetList: List<Tweet>? = null
+  private lateinit var tweetList: List<Tweet>
 
   init {
     this.tweetList = ArrayList<Tweet>()
@@ -27,24 +29,26 @@ class TweetAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
     val tweet: Tweet? = null
-    val childView = CompactTweetView(parent.context, tweet)
+    //val childView = CompactTweetView(parent.context, tweet)
+    val childView = LayoutInflater.from(parent.context)
+        .inflate(R.layout.list_item_tweet, parent, false)
 
     return TweetHolder(childView)
   }
 
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-    (holder as TweetHolder).bind(tweetList!![position])
+    (holder as TweetHolder).bind(tweetList[position])
   }
 
   override fun getItemCount(): Int {
-    return tweetList!!.size
+    return tweetList.size
   }
 
   private class TweetHolder constructor(itemView: View)
       : RecyclerView.ViewHolder(itemView) {
 
     fun bind(tweet: Tweet) {
-      (itemView as BaseTweetView).tweet = tweet
+      (itemView as TweetView).setTweet(tweet)
     }
 
   }
--- app/src/main/kotlin/com/htoyama/likit/ui/TweetView.kt
+++ app/src/main/kotlin/com/htoyama/likit/ui/TweetView.kt
@@ -2,18 +2,62 @@ package com.htoyama.likit.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
+import butterknife.bindView
+import com.htoyama.likit.R
+import com.squareup.picasso.Picasso
 
 import com.twitter.sdk.android.core.models.Tweet
 
 /**
  * Created by toyamaosamuyu on 2016/06/17.
  */
-class TweetView @JvmOverloads constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) : RelativeLayout(context, attrs, defStyleAttr) {
+class TweetView
+    @JvmOverloads constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0)
+    : RelativeLayout(context, attrs, defStyleAttr) {
+
   private var tweet: Tweet? = null
+
+  val avatorIv: ImageView by bindView(R.id.tw__tweet_author_avatar)
+  val fullNameTv: TextView by bindView(R.id.tw__tweet_author_full_name)
+  val screenNameTv: TextView by bindView(R.id.tw__tweet_author_screen_name)
+  val contentTv: TextView by bindView(R.id.tw__tweet_text)
+  val timestampTv: TextView by bindView(R.id.tw__tweet_timestamp)
 
   fun setTweet(tweet: Tweet) {
     this.tweet = tweet
+    render()
+  }
+
+  private fun render() {
+    val tweet = displayTweet() ?: return
+
+    Picasso.with(context)
+        .load(avatorUrl(tweet.user.profileImageUrlHttps))
+        .into(avatorIv)
+
+    fullNameTv.text = tweet.user.name
+    screenNameTv.text = tweet.user.screenName
+    contentTv.text = tweet.text
+
+  }
+
+  private fun avatorUrl(avatorUrl: String): String {
+    return avatorUrl.replace("_normal", "_reasonably_small")
+  }
+
+  override fun onFinishInflate() {
+    super.onFinishInflate()
+  }
+
+  private fun displayTweet(): Tweet? {
+    if (tweet == null || (tweet as Tweet).retweetedStatus == null) {
+      return tweet
+    } else {
+      return (tweet as Tweet).retweetedStatus
+    }
   }
 
 }
