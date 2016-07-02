913c0f883945ceded70ff8bbbe94b1c7c5e9ae87
Haruyoshi Toyama <duane0728@gmail.com>
2016-07-03 03:26:26 +0900

    remove some methods that needs model in twitter-kit

--- app/src/main/kotlin/com/htoyama/likit/ui/TweetView.kt
+++ app/src/main/kotlin/com/htoyama/likit/ui/TweetView.kt
@@ -1,28 +1,17 @@
 package com.htoyama.likit.ui
 
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.support.v4.content.res.ResourcesCompat
-import android.text.TextUtils
 import android.util.AttributeSet
-import android.util.Log
-import android.view.View
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import butterknife.bindView
 import com.htoyama.likit.R
-import com.htoyama.likit.model.tweet.TweetFactory
+import com.htoyama.likit.model.tweet.Tweet
 import com.htoyama.likit.util.*
 import com.htoyama.toGone
 import com.htoyama.toVisible
 import com.squareup.picasso.Picasso
-import com.twitter.sdk.android.core.models.MediaEntity
-
-import com.twitter.sdk.android.core.models.Tweet
-import com.twitter.sdk.android.tweetui.internal.SpanClickHandler
-import com.twitter.sdk.android.tweetui.internal.TweetMediaUtils
 
 /**
  * Created by toyamaosamuyu on 2016/06/17.
@@ -47,161 +36,36 @@ class TweetView
   }
 
   private fun render() {
-    val factory = TweetFactory()
+    tweet?: return
+    val tweet: Tweet = tweet as Tweet
 
-    val tweet = displayTweet() ?: return
-    var my = factory.from(tweet)
     Picasso.with(context)
-        .load(my.user.avatorUrl)
+        .load(tweet.user.avatorUrl)
         .into(avatorIv)
 
-    fullNameTv.text = my.user.name
-    screenNameTv.text = my.user.screenName
+    fullNameTv.text = tweet.user.name
+    screenNameTv.text = tweet.user.screenName
     timestampTv.text = TweetDateUtils.getRelativeTimeString(
-        resources, System.currentTimeMillis(), my.createdAt)
+        resources, System.currentTimeMillis(), tweet.createdAt)
 
     val linklifier =  TweetTextLinklifer()
-    contentTv.text = linklifier.linklifyText(my,
+    contentTv.text = linklifier.linklifyText(tweet,
         context.getColor(R.color.tweet_action_light_highlight_color), listener)
     contentTv.movementMethod = LinkTouchMovementMethod.instance
 
-    if (my.photos.isEmpty()) {
+    if (tweet.photos.isEmpty()) {
       mediaIv.toGone()
+      return
     }
-    for (photo in my.photos) {
+
+    for (photo in tweet.photos) {
       mediaIv.setImageSize(photo.sizes.medium.width, photo.sizes.medium.height);
-      mediaIv.visibility = View.VISIBLE
+      mediaIv.toVisible()
       Picasso.with(context)
           .load(photo.url)
           .into(mediaIv)
       break
     }
-
-
-    /*
-    Picasso.with(context)
-        .load(avatorUrl(tweet.user.profileImageUrlHttps))
-        .into(avatorIv)
-
-    setFullName(tweet)
-    setScreenName(tweet)
-    setContent(tweet)
-    setTimeStamp(tweet)
-    setMedia(tweet)
-    */
-  }
-
-  private fun setContent(tweet: Tweet?) {
-    if (tweet == null) {
-      contentTv.text = ""
-      contentTv.toGone()
-      return
-    }
-
-    val formatTweetText = TweetTextUtils.formatTweetText(tweet)
-    val linkColor = ResourcesCompat.getColor(resources, R.color.tweet_text_link, null)
-    val linkHighLightColor = ResourcesCompat.getColor(resources, R.color.tweet_text_link_highlight, null)
-    val hasPhoto = TweetMediaUtils.hasPhoto(tweet)
-    /*
-    val text = TweetTextLinkifier.linkifyUrls(formatTweetText, linkClickListener,
-        hasPhoto, linkColor, linkHighLightColor)
-        */
-    val text = formatTweetText.text
-
-    if (text == null) {
-      contentTv.text = ""
-      contentTv.toGone()
-      return
-    }
-
-    contentTv.text = text
-    contentTv.toVisible()
-    SpanClickHandler.enableClicksOnSpans(contentTv)
-  }
-
-  private fun setFullName(tweet: Tweet?) {
-    if (tweet != null && tweet.user != null && tweet.user.name != null) {
-      fullNameTv.text = tweet.user.name
-    } else {
-      fullNameTv.text = ""
-    }
-  }
-
-  private fun setScreenName(tweet: Tweet?) {
-    if (tweet != null && tweet.user != null && tweet.user.screenName!= null) {
-      screenNameTv.text = formatScreenName(tweet.user.screenName)
-    } else {
-      screenNameTv.text = ""
-    }
-  }
-
-  private fun formatScreenName(screenName: CharSequence): CharSequence {
-    return if (TextUtils.isEmpty(screenName)) {
-      ""
-    } else if (screenName[0] == '@') {
-      screenName
-    } else {
-      "@" + screenName
-    }
-  }
-
-  private fun setTimeStamp(tweet: Tweet) {
-    val createdAt = tweet.createdAt
-    val timeStamp: String
-    if (createdAt != null && TweetDateUtils.isValidTimestamp(createdAt)) {
-      timeStamp = TweetDateUtils.getRelativeTimeString(
-          resources, System.currentTimeMillis(), TweetDateUtils.apiTimeToLong(createdAt))
-    } else {
-      timeStamp = ""
-    }
-
-    timestampTv.text = timeStamp
-  }
-
-  private fun setMedia(tweet: Tweet?) {
-    if (tweet == null || !
-    TweetMediaUtils.hasPhoto(tweet)) {
-      mediaIv.visibility = View.GONE
-      return
-    }
-
-    val photoEntity = TweetMediaUtils.getPhotoEntity(tweet)
-    if (photoEntity != null && photoEntity.sizes != null && photoEntity.sizes.medium != null) {
-        mediaIv.setImageSize(photoEntity.sizes.medium.w, photoEntity.sizes.medium.h);
-    }
-    mediaIv.visibility = View.VISIBLE
-    Picasso.with(context)
-        .load(photoEntity.mediaUrlHttps)
-        .into(mediaIv)
-  }
-
-  private fun avatorUrl(avatorUrl: String): String {
-    return avatorUrl.replace("_normal", "_reasonably_small")
-  }
-
-  private fun displayTweet(): Tweet? {
-    if (tweet == null || (tweet as Tweet).retweetedStatus == null) {
-      return tweet
-    } else {
-      return (tweet as Tweet).retweetedStatus
-    }
-  }
-
-  interface OnItemClickListener {
-    fun onUrlClicked(url: String)
-    fun onPhotoClicked()
-    fun onTweetClick()
-  }
-
-  private val linkClickListener: LinkClickListener = object : LinkClickListener {
-    override fun onUrlClicked(url: String?) {
-      val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url));
-      context.startActivity(intent)
-    }
-
-    override fun onPhotoClicked(mediaEntity: MediaEntity?) {
-    }
-
   }
 
 }
