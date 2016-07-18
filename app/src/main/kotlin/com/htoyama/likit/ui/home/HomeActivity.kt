d60420234a2dab87b881d6a40d1e1c7f27d01564
Haruyoshi Toyama <duane0728@gmail.com>
2016-07-18 18:41:43 +0900

    Convert HomeActivity.java to kotlin

---
+++ app/src/main/kotlin/com/htoyama/likit/ui/home/HomeActivity.kt
@@ -0,0 +1,33 @@
+package com.htoyama.likit.ui.home
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+import com.htoyama.likit.R
+
+class HomeActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_home)
+    val toolbar = findViewById(R.id.toolbar) as Toolbar?
+    setSupportActionBar(toolbar)
+
+    val fab = findViewById(R.id.fab) as FloatingActionButton?
+    fab!!.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+
+    val tabLayout = findViewById(R.id.home_tablayout) as TabLayout?
+    tabLayout!!.addTab(tabLayout.newTab().setText("Tags"))
+    tabLayout.addTab(tabLayout.newTab().setText("Liked"))
+    val pager = findViewById(R.id.home_pager) as ViewPager?
+    val adapter = HomePagerAdapter(supportFragmentManager)
+    pager!!.adapter = adapter
+    pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+  }
+
+}
