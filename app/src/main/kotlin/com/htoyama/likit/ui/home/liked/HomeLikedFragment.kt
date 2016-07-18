a335d74e103d143395670dd707b12b039b9162b3
Haruyoshi Toyama <duane0728@gmail.com>
2016-07-18 17:59:58 +0900

    Convert Fragment.java to Fragment.kt

---
+++ app/src/main/kotlin/com/htoyama/likit/ui/home/liked/HomeLikedFragment.kt
@@ -0,0 +1,21 @@
+package com.htoyama.likit.ui.home.liked
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.htoyama.likit.R
+
+/**
+ *
+ */
+class HomeLikedFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+    return inflater!!.inflate(R.layout.fragment_home_liked, container, false)
+  }
+
+}
