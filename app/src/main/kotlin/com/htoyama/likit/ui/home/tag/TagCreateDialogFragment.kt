1952265d0e14024adb9bd3af1d99a83cc9baabf4
Haruyoshi Toyama <duane0728@gmail.com>
2016-08-11 05:05:33 +0900

    Organize Dialog functions

--- app/src/main/kotlin/com/htoyama/likit/ui/home/tag/TagCreateDialogFragment.kt
+++ app/src/main/kotlin/com/htoyama/likit/ui/home/tag/TagCreateDialogFragment.kt
@@ -3,9 +3,18 @@ package com.htoyama.likit.ui.home.tag
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.htoyama.likit.R
+import com.jakewharton.rxbinding.widget.RxTextView
+import rx.Subscription
+import rx.subscriptions.Subscriptions
 
 /**
  * A [DialogFragment] that has responsi
@@ -23,20 +32,85 @@ class TagCreateDialogFragment : DialogFragment() {
     fun onTagCreateButtonClick()
   }
 
-  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    return AlertDialog.Builder(context)
-        .setTitle("Create a tag")
-        .setMessage("foo")
-        .setPositiveButton("Create", { dialog, which ->
-          if (which == DialogInterface.BUTTON_POSITIVE) {
-            val activity = activity;
-            if (activity is OnClickListener) {
-              activity.onTagCreateButtonClick()
-            }
-          }
-        })
-        .setNegativeButton("Cancel", null)
-        .create()
+  private var sub: Subscription = Subscriptions.empty()
+  private lateinit var inputLayout: TextInputLayout
+
+  private val tagNameMaxCount: Int by lazy { resources.getInteger(R.integer.tag_name_max_count) }
+  private val countOverErrorMessage: String by lazy {
+    resources.getString(R.string.tag_create_count_over_error)
   }
+  private val dialogTitle: String by lazy { resources.getString(R.string.tag_create_dialog_title) }
+  private val dialogPositive: String by lazy {
+    resources.getString(R.string.tag_create_dialog_positive)
+  }
+  private val dialogNegative: String by lazy {
+    resources.getString(R.string.tag_create_dialog_negative)
+  }
+
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    val dialog = AlertDialog.Builder(context)
+        .setTitle(dialogTitle)
+        .setView(createContentView())
+        .setPositiveButton(dialogPositive, null)
+        .setNegativeButton(dialogNegative, null)
+        .create()
+
+    return dialog
+  }
+
+  override fun onStart() {
+    super.onStart()
+
+    val button = (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE)
+    button?.setOnClickListener {
+      val length = getTextLength()
+      val isNoInput = length <= 0
+      val isOverLength = tagNameMaxCount < length
+
+      if (isNoInput) {
+        Toast.makeText(context, R.string.tag_create_no_input_error, Toast.LENGTH_SHORT).show()
+      } else if (isOverLength) {
+        Toast.makeText(context, countOverErrorMessage, Toast.LENGTH_SHORT).show()
+      } else {
+        (activity as OnClickListener).onTagCreateButtonClick()
+        dismiss()
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    sub.unsubscribe()
+  }
+
+  private fun createContentView(): View {
+    val view = LayoutInflater.from(context).inflate(R.layout.dialog_tag_create, null)
+    inputLayout = view.findViewById(R.id.tag_create_input_layout) as TextInputLayout
+    val editText: EditText = view.findViewById(R.id.tag_create_edit_text) as EditText
+
+    val countOverErrorMessage = resources.getString(R.string.tag_create_count_over_error)
+    sub = RxTextView.textChangeEvents(editText)
+        .map {event ->
+          val length = event.text().length
+          length > tagNameMaxCount
+        }
+        .subscribe { isOver ->
+          if (isOver) {
+            inputLayout.error = countOverErrorMessage
+            inputLayout.isErrorEnabled = true
+          } else {
+            inputLayout.error = ""
+            inputLayout.isErrorEnabled = false
+          }
+        }
+
+    return view
+  }
+
+  private fun getTextLength(): Int = inputLayout
+        .editText
+        ?.text
+        .toString()
+        .length
 
 }
