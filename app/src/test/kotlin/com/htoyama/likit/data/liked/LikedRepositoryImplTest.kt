ddbb163623f5f194bbf8e535c601552fe1eb0998
Haruyoshi Toyama <duane0728@gmail.com>
2016-08-29 02:12:02 +0900

    Fix unit tests

--- app/src/test/kotlin/com/htoyama/likit/data/liked/LikedRepositoryImplTest.kt
+++ app/src/test/kotlin/com/htoyama/likit/data/liked/LikedRepositoryImplTest.kt
@@ -1,12 +1,9 @@
 package com.htoyama.likit.data.liked
 
-import android.util.LongSparseArray
 import com.htoyama.likit.TweetBuilder
 import com.htoyama.likit.data.liked.tweet.LikedTweetDao
 import com.htoyama.likit.domain.liked.LikedBuilder
 import com.htoyama.likit.domain.liked.LikedTweet
-import com.htoyama.likit.domain.tag.Tag
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -19,16 +16,14 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricGradleTestRunner
 import rx.observers.TestSubscriber
 
-@RunWith(RobolectricGradleTestRunner::class)
 class LikedRepositoryImplTest {
   @Mock lateinit private var dao: LikedTweetDao
   @Mock lateinit private var gateway: LikedRealmGateway
-  @Mock lateinit private var factory: LikedFactory
   lateinit private var impl: LikedRepositoryImpl
 
   @Before fun setUp() {
     MockitoAnnotations.initMocks(this)
-    impl = LikedRepositoryImpl(dao, gateway, factory)
+    impl = LikedRepositoryImpl(dao, gateway)
   }
 
   @Test fun find() {
@@ -38,22 +33,16 @@ class LikedRepositoryImplTest {
     When(dao.getTweetList(1, 1))
         .thenReturn(tweetListObs)
 
-    val tagTable = LongSparseArray<List<Tag>>(1)
-    tagTable.append(1, ArrayList<Tag>())
-    When(gateway.getBy(tweetList))
-        .thenReturn(tagTable)
-
     val expected = Arrays.asList(LikedBuilder().build())
-    When(factory.createFrom(tagTable, tweetList))
+    When(gateway.getBy(tweetList))
         .thenReturn(expected)
 
     val actual = impl.find(1, 1)
 
     val sub = TestSubscriber<List<LikedTweet>>();
-    actual.subscribe(sub);
-    sub.awaitTerminalEvent();
-    sub.assertNoErrors();
-    sub.assertCompleted();
+    actual.subscribe(sub)
+    sub.awaitTerminalEvent()
+    sub.assertCompleted()
     val events = sub.onNextEvents
     assertThat(events).hasSize(1)
     assertThat(events[0]).isEqualTo(expected)
