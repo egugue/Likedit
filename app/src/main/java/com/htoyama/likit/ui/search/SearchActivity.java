8551cceab43d89c9537c11c0fc701064c1aa2907
Haruyoshi Toyama <duane0728@gmail.com>
2016-09-19 02:23:43 +0900

    Remove unnecessary imports

--- app/src/main/java/com/htoyama/likit/ui/search/SearchActivity.java
+++ app/src/main/java/com/htoyama/likit/ui/search/SearchActivity.java
@@ -4,12 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.htoyama.likit.R;
 import com.htoyama.likit.databinding.ActivitySearchBinding;
