93ce5087f663603cd91d23235710847c9b811cae
Haruyoshi Toyama <duane0728@gmail.com>
2016-09-21 21:09:06 +0900

    Modify using string.xml

--- app/src/main/java/com/htoyama/likit/ui/search/Header.java
+++ app/src/main/java/com/htoyama/likit/ui/search/Header.java
@@ -2,15 +2,18 @@ package com.htoyama.likit.ui.search;
 
 import android.content.Context;
 
+import com.htoyama.likit.R;
+
 public enum Header {
+
   TAG {
     @Override String displayingText(Context context) {
-      return "タグ";
+      return context.getString(R.string.search_assist_header_tag);
     }
   },
   USER {
     @Override String displayingText(Context context) {
-      return "ユーザー";
+      return context.getString(R.string.search_assist_header_tag);
     }
   };
 
