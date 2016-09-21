6e097bc8b19bc2b48bf426ef077644f882ee11ef
Haruyoshi Toyama <duane0728@gmail.com>
2016-09-21 20:03:48 +0900

    [WIP] Implement to show search assist

--- app/src/main/java/com/htoyama/likit/ui/search/SearchActivity.java
+++ app/src/main/java/com/htoyama/likit/ui/search/SearchActivity.java
@@ -4,15 +4,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.htoyama.likit.R;
 import com.htoyama.likit.databinding.ActivitySearchBinding;
+import com.htoyama.likit.ui.common.DividerItemDecoration;
+import com.htoyama.likit.ui.common.activity.BaseRxActivity;
+import com.jakewharton.rxbinding.widget.RxTextView;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+public class SearchActivity extends BaseRxActivity implements Presenter.View {
 
   private ActivitySearchBinding binding;
+  @Inject Presenter presenter;
+  @Inject AssistAdapter adapter;
 
   public static Intent createIntent(Context context) {
     return new Intent(context, SearchActivity.class);
@@ -23,7 +35,40 @@ public class SearchActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
 
+    SearchComponent.Initializer
+        .init(this)
+        .inject(this);
+
     initToolbar();
+    initList();
+    presenter.setView(this);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    presenter.unsubscribe();
+  }
+
+  @Override public void showAssist(@NonNull Assist assist) {
+    Log.d("ーーー", assist.toString());
+    adapter.setAssist(assist);
+  }
+
+  private void initList() {
+    RecyclerView listView = binding.searchAssistList;
+    listView.setAdapter(adapter);
+    listView.addItemDecoration(new DividerItemDecoration(this));
+    listView.setLayoutManager(new LinearLayoutManager(this));
+
+    RxTextView.afterTextChangeEvents(binding.searchQuery)
+        .compose(bindToLifecycle())
+        .throttleLast(500, TimeUnit.MILLISECONDS)
+        .map(event -> event.editable().toString())
+        .distinctUntilChanged()
+        .filter(query -> query.length() > 3)
+        .subscribe(query -> {
+          presenter.loadAssist(query);
+        });
   }
 
   private void initToolbar() {
