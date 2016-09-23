a11f1d8b7772285eb7c480cb5d928c8da7a079da
Haruyoshi Toyama <duane0728@gmail.com>
2016-09-23 23:47:59 +0900

    Fix test method name

---
+++ app/src/test/java/com/htoyama/likit/data/liked/tweet/LikedTweetDaoTest.java
@@ -0,0 +1,127 @@
+package com.htoyama.likit.data.liked.tweet;
+
+import com.htoyama.likit.TweetBuilder;
+import com.htoyama.likit.common.Irrelevant;
+import com.htoyama.likit.data.common.net.FavoriteService;
+import com.htoyama.likit.data.liked.LikedRealmGateway;
+import com.htoyama.likit.data.liked.tweet.cache.LikedTweetCacheGateway;
+import com.htoyama.likit.data.tweet.TweetMapper;
+import com.htoyama.likit.domain.tweet.Tweet;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LikedTweetDaoTest {
+  @Mock FavoriteService favoriteService;
+  @Mock LikedTweetCacheGateway cacheGateway;
+  @Mock LikedRealmGateway likedRealmGateway;
+  private LikedTweetDao dao;
+
+  @Before public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    dao = new LikedTweetDao(favoriteService, cacheGateway, likedRealmGateway, new TweetMapper());
+  }
+
+  @Test public void getTweetList_whenHavingCache() {
+    // given
+    int page = 1;
+    int count = 1;
+    List<Tweet> fromCache = Arrays.asList(new TweetBuilder().build());
+    List<com.twitter.sdk.android.core.models.Tweet> fromNetwork = Collections.emptyList();
+
+    when(cacheGateway.getList(page, count))
+        .thenReturn(Single.just(fromCache));
+    when(favoriteService.list(null, null, count, null, null, true, page))
+        .thenReturn(callFrom(fromNetwork));
+
+    // when
+    TestObserver<List<Tweet>> test = dao.getTweetList(page, count).test();
+    test.awaitTerminalEvent();
+
+    // then
+    test.assertValue(fromCache);
+    verify(likedRealmGateway, never())
+        .insertAsContainingNoTag(any(fromCache.getClass()));
+  }
+
+  @Ignore("Creating Tweet model is hard.")
+  @Test public void getTweetList_whenNoCache() {
+    // given
+    int page = 1;
+    int count = 1;
+    List<Tweet> noCache = Collections.emptyList();
+    List<com.twitter.sdk.android.core.models.Tweet> fromNetwork = Arrays.asList(
+
+    );
+
+    when(cacheGateway.getList(page, count))
+        .thenReturn(Single.just(noCache));
+    when(favoriteService.list(null, null, count, null, null, true, page))
+        .thenReturn(callFrom(fromNetwork));
+
+    // when
+    TestObserver<List<Tweet>> test = dao.getTweetList(page, count).test();
+    test.awaitTerminalEvent();
+
+    // then
+    test.assertValueCount(1);
+    verify(likedRealmGateway)
+        .insertAsContainingNoTag(any(noCache.getClass()));
+
+  }
+
+  private <T> Call<T> callFrom(T response) {
+    return new Call<T>() {
+
+      @Override public Response<T> execute() throws IOException {
+        return Response.success(response);
+      }
+
+      @Override public void enqueue(Callback<T> callback) {
+
+      }
+
+      @Override public boolean isExecuted() {
+        return false;
+      }
+
+      @Override public void cancel() {
+
+      }
+
+      @Override public boolean isCanceled() {
+        return false;
+      }
+
+      @Override public Call<T> clone() {
+        return null;
+      }
+
+      @Override public Request request() {
+        return null;
+      }
+    };
+  }
+
+}
