5ca53d887e5bc36d5f5fa834c79b8d2e2276157d
Haruyoshi Toyama <duane0728@gmail.com>
2016-10-03 05:13:53 +0900

    Remove unnecessary method

--- app/src/main/java/com/htoyama/likit/common/Contract.java
+++ app/src/main/java/com/htoyama/likit/common/Contract.java
@@ -7,28 +7,6 @@ public final class Contract {
 
   /**
    * Check the given value must be not null.
-   * If null, throw {@link NullPointerException} having no message.
-   *
-   * @throws NullPointerException the given value is null.
-   * @see #requireNotNull(Object, String)
-   */
-  public static <T> T requireNotNull(T value) {
-    if (value == null) {
-      NullPointerException npe = new NullPointerException("");
-
-      StackTraceElement[] source = npe.getStackTrace();
-      StackTraceElement[] updated = new StackTraceElement[source.length - 1];
-      System.arraycopy(source, 1, updated, 0, updated.length);
-
-      npe.setStackTrace(updated);
-      throw npe;
-    }
-
-    return value;
-  }
-
-  /**
-   * Check the given value must be not null.
    * If null, throw {@link NullPointerException} which has message containing the given valueName.
    *
    * @throws NullPointerException the given value is null.
