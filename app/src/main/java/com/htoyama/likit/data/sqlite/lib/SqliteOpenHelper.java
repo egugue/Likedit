2564a9ef9a5185a39f9cfd087e775bc68b73c74e
Haruyoshi Toyama <duane0728@gmail.com>
2017-03-27 22:04:40 +0900

    Add SqliteOpenHelper

---
+++ app/src/main/java/com/htoyama/likit/data/sqlite/lib/SqliteOpenHelper.java
@@ -0,0 +1,26 @@
+package com.htoyama.likit.data.sqlite.lib;
+
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.htoyama.likit.data.sqlite.TweetModel;
+import com.htoyama.likit.data.sqlite.UserModel;
+import com.htoyama.likit.data.sqlite.entity.TweetEntity;
+import com.htoyama.likit.data.sqlite.entity.UserEntity;
+
+public class SqliteOpenHelper extends SQLiteOpenHelper {
+
+  public SqliteOpenHelper(Application app) {
+    super(app, "likedit", null, 1);
+  }
+
+  @Override public void onCreate(SQLiteDatabase db) {
+    db.execSQL(TweetModel.CREATE_TABLE);
+    db.execSQL(UserModel.CREATE_TABLE);
+  }
+
+  @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+  }
+}
