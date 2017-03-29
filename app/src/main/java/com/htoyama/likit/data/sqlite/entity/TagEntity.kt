c25d68681c89dfccdda182ddbd59b887d6190404
Haruyoshi Toyama <duane0728@gmail.com>
2017-03-30 00:47:00 +0900

    Add tag table

---
+++ app/src/main/java/com/htoyama/likit/data/sqlite/entity/TagEntity.kt
@@ -0,0 +1,14 @@
+package com.htoyama.likit.data.sqlite.entity
+
+import com.htoyama.likit.data.sqlite.TagModel
+
+data class TagEntity(
+    val id: Long,
+    val name: String,
+    val created: Long
+) : TagModel {
+
+  override fun id(): Long = id
+  override fun name(): String = name
+  override fun created(): Long = created
+}
