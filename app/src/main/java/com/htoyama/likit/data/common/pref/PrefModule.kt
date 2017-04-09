6a8f17a017e78e2e84ccdd7d3bad9770246cd362
Haruyoshi Toyama <duane0728@gmail.com>
2017-04-09 18:41:25 +0900

    Add AppSetting

--- app/src/main/java/com/htoyama/likit/data/common/pref/PrefModule.kt
+++ app/src/main/java/com/htoyama/likit/data/common/pref/PrefModule.kt
@@ -3,6 +3,7 @@ package com.htoyama.likit.data.common.pref
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.f2prateek.rx.preferences2.RxSharedPreferences
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -11,6 +12,9 @@ import javax.inject.Singleton
 
   @Provides @Singleton fun pref(app: Application): SharedPreferences =
       app.getSharedPreferences("likit", Context.MODE_PRIVATE)
+
+  @Provides @Singleton fun rxPrefs(pref: SharedPreferences) =
+      RxSharedPreferences.create(pref)
 
 }
 
