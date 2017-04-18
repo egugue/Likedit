773165e56100c4e4ad9f58cb3695f5c0a100da95
Haruyoshi Toyama <duane0728@gmail.com>
2017-04-19 01:36:35 +0900

    Change type to Any, then not to depend on a specific type

--- app/src/test/java/com/htoyama/likit/common/IrrelevantTest.java
+++ app/src/test/java/com/htoyama/likit/common/IrrelevantTest.java
@@ -7,8 +7,8 @@ import static com.google.common.truth.Truth.assertThat;
 public class IrrelevantTest {
 
   @Test public void equals() {
-    Irrelevant instance1 = Irrelevant.get();
-    Irrelevant instance2 = Irrelevant.get();
+    Object instance1 = Irrelevant.get();
+    Object instance2 = Irrelevant.get();
 
     assertThat(instance1).isEqualTo(instance2);
   }
