03ddfb8f7c84784f4fa6b9eb381dea537b0c6ff3
Haruyoshi Toyama <duane0728@gmail.com>
2017-04-24 02:23:39 +0900

    Remove unused codes

--- app/src/main/java/com/htoyama/likit/background/sync/SyncComponent.kt
+++ app/src/main/java/com/htoyama/likit/background/sync/SyncComponent.kt
@@ -2,12 +2,10 @@ package com.htoyama.likit.background.sync
 
 import com.htoyama.likit.AppComponent
 import com.htoyama.likit.background.SerivceScope
-import com.htoyama.likit.ui.home.HomeScope
 import dagger.Component
 
-
 @SerivceScope
-@Component(dependencies = arrayOf(AppComponent::class), modules = arrayOf(SyncModule::class))
+@Component(dependencies = arrayOf(AppComponent::class))
 interface SyncComponent {
   fun inject(s: TweetSyncService)
 }
