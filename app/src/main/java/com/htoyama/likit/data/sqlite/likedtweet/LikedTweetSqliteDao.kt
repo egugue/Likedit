0f822d583d10844fea6e4ff895e866c1325c760c
Haruyoshi Toyama <duane0728@gmail.com>
2017-04-30 22:33:53 +0900

    Add class and a method

---
+++ app/src/main/java/com/htoyama/likit/data/sqlite/likedtweet/LikedTweetSqliteDao.kt
@@ -0,0 +1,48 @@
+package com.htoyama.likit.data.sqlite.likedtweet
+
+import com.htoyama.likit.common.Contract
+import com.htoyama.likit.data.sqlite.IdMap
+import com.htoyama.likit.data.sqlite.relation.TweetTagRelation
+import com.htoyama.likit.data.sqlite.relation.TweetTagRelationTableGateway
+import com.htoyama.likit.domain.likedtweet.LikedTweet
+import io.reactivex.Observable
+import javax.inject.Inject
+
+/**
+ * A Data Access Object that handles liked Tweets.
+ */
+class LikedTweetSqliteDao @Inject constructor(
+    private val likedTweetGateway: LikedTweetTableGateway,
+    private val relationGateway: TweetTagRelationTableGateway
+) {
+
+  /**
+   * Select some [LikedTweet]s by the given page and per page
+   *
+   * @param page
+   *     the number of page, which must be a positive integer
+   * @param perPage
+   *    the number of tweets to retrieve per a page,
+   *    which must be between 1 and 200
+   */
+  fun select(page: Int, perPage: Int): Observable<List<LikedTweet>> {
+    Contract.require(page > 0, "0 < page required but it was $page")
+    Contract.require(perPage in 1..200, "0 < perPage < 201 required but it was $perPage")
+
+    val tweetEntityList = likedTweetGateway.selectTweet(page, perPage)
+    val tweetEntityListAndIdMap = tweetEntityList
+        .flatMap(
+            { selectRelationsByTweetEntityList(it) },
+            { entityList, relations -> entityList to IdMap.basedOnTweetId(relations) })
+
+    return tweetEntityListAndIdMap
+        .map { (tweetEntityList, idMap) ->
+          tweetEntityList.map { it.toLikedTweet(idMap) }
+        }
+  }
+
+  private fun selectRelationsByTweetEntityList(tweetList: List<FullLikedTweetEntity>): Observable<List<TweetTagRelation>> {
+    val idList = tweetList.map { it.tweet.id }
+    return relationGateway.selectRelationsByTweetIdList(idList)
+  }
+}
