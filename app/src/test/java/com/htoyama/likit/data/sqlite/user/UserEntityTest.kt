6a0fa5fb0f46cd5f67b3ba562e81de2d384cc1be
Haruyoshi Toyama <duane0728@gmail.com>
2017-04-30 19:58:29 +0900

    Add factory methods

--- app/src/test/java/com/htoyama/likit/data/sqlite/user/UserEntityTest.kt
+++ app/src/test/java/com/htoyama/likit/data/sqlite/user/UserEntityTest.kt
@@ -1,8 +1,35 @@
 package com.htoyama.likit.data.sqlite.user
 
-import org.junit.Assert.*
+import com.htoyama.likit.data.sqlite.userEntity
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import com.htoyama.likit.user
 
-/**
- * Created by htoyama on 2017/04/30.
- */
-class UserEntityTest
+class UserEntityTest {
+
+  @Test fun `convert into User`() {
+    val entity = userEntity()
+
+    val actual = entity.toUser()
+
+    actual.run {
+      assertThat(id).isEqualTo(entity.id)
+      assertThat(name).isEqualTo(entity.name)
+      assertThat(screenName).isEqualTo(entity.screenName)
+      assertThat(avatorUrl).isEqualTo(entity.avatarUrl)
+    }
+  }
+
+  @Test fun `create from User`() {
+    val user = user()
+
+    val actual = UserEntity.from(user)
+
+    actual.run {
+      assertThat(id).isEqualTo(user.id)
+      assertThat(name).isEqualTo(user.name)
+      assertThat(screenName).isEqualTo(user.screenName)
+      assertThat(avatarUrl).isEqualTo(user.avatorUrl)
+    }
+  }
+}
