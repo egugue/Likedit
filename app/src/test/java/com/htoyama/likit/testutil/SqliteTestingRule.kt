07d58b56f095a438bfbc964088b31671eb44c68b
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-03 05:44:56 +0900

    Add SqliteTestingRule

---
+++ app/src/test/java/com/htoyama/likit/testutil/SqliteTestingRule.kt
@@ -0,0 +1,33 @@
+package com.htoyama.likit.testutil
+
+import com.htoyama.likit.data.sqlite.briteDatabaseForTest
+import com.squareup.sqlbrite.BriteDatabase
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import org.robolectric.RuntimeEnvironment
+
+/**
+ * A [TestRule] to execute regular process at tear down method when testing about SQLite.
+ */
+class SqliteTestingRule : TestRule{
+
+  /**
+   * a [BriteDatabase] for each test method
+   */
+  lateinit var briteDB: BriteDatabase
+
+  override fun apply(base: Statement, description: Description): Statement {
+    return object : Statement() {
+      override fun evaluate() {
+        try {
+          briteDB = briteDatabaseForTest()
+          base.evaluate()
+        } finally {
+          briteDB.close()
+          RuntimeEnvironment.application.deleteDatabase("likedit")
+        }
+      }
+    }
+  }
+}
