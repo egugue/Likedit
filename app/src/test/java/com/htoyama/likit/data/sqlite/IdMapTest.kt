a36a69b963df764a27c51aa6585e91977f8c7d0b
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-03 23:22:06 +0900

    Change test data

--- app/src/test/java/com/htoyama/likit/data/sqlite/IdMapTest.kt
+++ app/src/test/java/com/htoyama/likit/data/sqlite/IdMapTest.kt
@@ -9,13 +9,13 @@ class IdMapTest {
     val relationsList = listOf(
         tweetTagRelation(tweetId = 1, tagId = 1),
         tweetTagRelation(tweetId = 1, tagId = 2),
-        tweetTagRelation(tweetId = 2, tagId = 1)
+        tweetTagRelation(tweetId = 3, tagId = 1)
     )
 
     val actual = IdMap.basedOnTweetId(relationsList)
 
     assertThat(actual.getOrEmptyList(1)).containsExactly(1L, 2L)
-    assertThat(actual.getOrEmptyList(2)).containsExactly(1L)
+    assertThat(actual.getOrEmptyList(3)).containsExactly(1L)
   }
 
   @Test fun `get empty list if the given key(tweet id) is not present`() {
@@ -27,13 +27,13 @@ class IdMapTest {
     val relationsList = listOf(
         tweetTagRelation(tagId = 1, tweetId = 1),
         tweetTagRelation(tagId = 1, tweetId = 2),
-        tweetTagRelation(tagId = 2, tweetId = 1)
+        tweetTagRelation(tagId = 3, tweetId = 1)
     )
 
     val actual = IdMap.basedOnTagId(relationsList)
 
     assertThat(actual.getOrEmptyList(1)).containsExactly(1L, 2L)
-    assertThat(actual.getOrEmptyList(2)).containsExactly(1L)
+    assertThat(actual.getOrEmptyList(3)).containsExactly(1L)
   }
 
   @Test fun `get empty list if the given key(tag id) is not present`() {
