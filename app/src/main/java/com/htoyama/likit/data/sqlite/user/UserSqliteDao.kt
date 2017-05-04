bfa23a390570682b0aa7977bd8f8eb79095f9a4b
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-05 05:45:31 +0900

    Add document

--- app/src/main/java/com/htoyama/likit/data/sqlite/user/UserSqliteDao.kt
+++ app/src/main/java/com/htoyama/likit/data/sqlite/user/UserSqliteDao.kt
@@ -8,6 +8,9 @@ class UserSqliteDao @Inject constructor(
     private val userGateway: UserTableGateway
 ){
 
+  /**
+   * Select some users which have name or screen name with part of the given each arg.
+   */
   fun searchByNameOrScreenName(name: String, screenName: String, limit: Int): Observable<List<User>> {
     return userGateway.selectByNameOrScreenName(name, screenName, limit)
         .map { it.map { it.toUser() } }
