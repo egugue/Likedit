4d2c425bc6c72a9accd368abc6b21e82d44e87a6
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-07 01:55:51 +0900

    Add a query which can retrieve all users

--- app/src/main/java/com/htoyama/licol/data/sqlite/user/UserTableGateway.kt
+++ app/src/main/java/com/htoyama/licol/data/sqlite/user/UserTableGateway.kt
@@ -3,6 +3,7 @@ package com.htoyama.licol.data.sqlite.user
 import com.htoyama.licol.common.extensions.toV2Observable
 import com.htoyama.licol.data.sqlite.lib.createQuery
 import com.htoyama.licol.data.sqlite.lib.escapeForQuery
+import com.htoyama.licol.data.sqlite.lib.toLimitAndOffset
 import com.squareup.sqlbrite.BriteDatabase
 import io.reactivex.Observable
 import javax.inject.Inject
@@ -10,6 +11,15 @@ import javax.inject.Inject
 class UserTableGateway @Inject constructor(
     private val db: BriteDatabase
 ) {
+
+  fun selectAll(page: Int, perPage: Int): Observable<List<UserEntity>> {
+    val (limit, offset) = (page to perPage).toLimitAndOffset()
+
+    val stmt = UserEntity.FACTORY.select_all(limit, offset)
+    return db.createQuery(stmt)
+        .mapToList { UserEntity.FACTORY.select_allMapper().map(it) }
+        .toV2Observable()
+  }
 
   fun selectByNameOrScreenName(name: String, screenName: String, limit: Int)
       : Observable<List<UserEntity>> {
