bc50269d64626c905c30e1bb166bdd7c5bc87c04
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-12 23:47:30 +0900

    Reformat

--- app/src/debug/java/com/htoyama/licol/DebugApp.java
+++ app/src/debug/java/com/htoyama/licol/DebugApp.java
@@ -25,9 +25,10 @@ public class DebugApp extends App {
     StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
         .detectDiskReads()
         .detectDiskWrites()
-        .detectNetwork()   // or .detectAll() for all detectable problems
+        .detectNetwork()
         .penaltyLog()
         .build());
+
     StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
         .detectLeakedSqlLiteObjects()
         .detectLeakedClosableObjects()
