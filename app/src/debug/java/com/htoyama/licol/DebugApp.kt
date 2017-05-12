5f9bf2941ae817513741276c0a7f2c161c57e61e
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-12 23:50:21 +0900

    Convert DebugApp into kotlin

---
+++ app/src/debug/java/com/htoyama/licol/DebugApp.kt
@@ -0,0 +1,49 @@
+package com.htoyama.licol
+
+import android.os.StrictMode
+
+import com.facebook.stetho.Stetho
+
+/**
+ * The application only using Debug mode.
+ */
+class DebugApp : App() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+    if (isNotUsingRobolectric()) {
+      // if using Robolectric in local unit test, thrown IOException.
+      // see https://github.com/facebook/stetho/issues/440
+      Stetho.initializeWithDefaults(this)
+    }
+
+    applyStrictMode()
+  }
+
+  private fun applyStrictMode() {
+    StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+        .detectDiskReads()
+        .detectDiskWrites()
+        .detectNetwork()
+        .penaltyLog()
+        .build())
+
+    StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+        .detectLeakedSqlLiteObjects()
+        .detectLeakedClosableObjects()
+        .detectActivityLeaks()
+        .penaltyLog()
+        .penaltyDeath()
+        .build())
+  }
+
+  private fun isNotUsingRobolectric(): Boolean {
+    try {
+      Class.forName("org.robolectric.RobolectricTestRunner")
+      return false
+    } catch (ignored: ClassNotFoundException) {
+      return true
+    }
+  }
+}
