fbacb6af4fc2d55a058522e57499f52d3eec20c2
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-14 09:28:34 +0900

    Enable to show user list

--- app/src/main/java/com/htoyama/licol/ui/home/user/HomeUserFragment.kt
+++ app/src/main/java/com/htoyama/licol/ui/home/user/HomeUserFragment.kt
@@ -2,12 +2,24 @@ package com.htoyama.licol.ui.home.user
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import butterknife.bindView
 import com.htoyama.licol.R
+import com.htoyama.licol.application.user.UserAppService
+import com.htoyama.licol.common.extensions.observeOnMain
+import com.htoyama.licol.common.extensions.subscribeOnIo
+import com.htoyama.licol.ui.common.StateLayout
 import com.htoyama.licol.ui.home.HomeActivity
+import com.htoyama.licol.ui.home.user.list.UserController
 import com.trello.rxlifecycle2.components.support.RxFragment
+import com.trello.rxlifecycle2.kotlin.bindToLifecycle
+import javax.inject.Inject
 
 /**
  * A fragment showing user's list
@@ -15,6 +27,7 @@ import com.trello.rxlifecycle2.components.support.RxFragment
 class HomeUserFragment : RxFragment() {
 
   companion object {
+    const val PER_PAGE = 2
 
     /** Create a [HomeUserFragment] */
     fun new(): HomeUserFragment {
@@ -22,19 +35,51 @@ class HomeUserFragment : RxFragment() {
     }
   }
 
+  @Inject lateinit var appService: UserAppService
+  @Inject lateinit var userController: UserController
+
+  private var page: Int = 1
+  private val stateLayout: StateLayout by bindView(R.id.home_user_state_layout)
+  private val listView: RecyclerView by bindView(R.id.home_user_list)
+  private val errorView: TextView by bindView(R.id.home_user_error_state)
+  private val emptyView: TextView by bindView(R.id.home_user_empty_state)
+  private val progressBar: ProgressBar by bindView(R.id.progressBar)
+
   override fun onAttach(context: Context) {
     super.onAttach(context)
     (activity as HomeActivity).component
         .inject(this)
   }
 
-  override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    val root = inflater!!.inflate(R.layout.fragment_home_tag, container, false)
+    val root = inflater.inflate(R.layout.fragment_home_tag, container, false)
     return root
   }
 
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+
+    initListView()
+
+    appService.getAllUsers(page, PER_PAGE)
+        .bindToLifecycle(this)
+        .subscribeOnIo()
+        .observeOnMain()
+        .subscribe(
+            { userList ->
+              userController.setData(userList, false)
+              stateLayout.showContent()
+            },
+            { error ->
+              error.printStackTrace()
+            }
+        )
+  }
+
+  private fun initListView() {
+    listView.adapter = userController.adapter
+    listView.layoutManager = LinearLayoutManager(activity)
+    userController.requestModelBuild()
   }
 }
---
+++ app/src/main/java/com/htoyama/licol/ui/home/user/list/UserController.kt
@@ -0,0 +1,61 @@
+package com.htoyama.licol.ui.home.user.list
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.airbnb.epoxy.EpoxyController
+import com.airbnb.epoxy.EpoxyHolder
+import com.airbnb.epoxy.EpoxyModelWithHolder
+import com.htoyama.licol.R
+import com.htoyama.licol.domain.user.User
+import javax.inject.Inject
+
+/**
+ * Created by htoyama on 2017/05/14.
+ */
+class UserController @Inject constructor() : EpoxyController() {
+  private var userList: List<User> = emptyList()
+  private var requireLoadingMore: Boolean = true
+
+  fun setData(l: List<User>, requireLoadingMore: Boolean) {
+    userList = l
+    this.requireLoadingMore = requireLoadingMore
+    requestModelBuild()
+  }
+
+  override fun buildModels() {
+    userList.forEach {
+      UserModel(it)
+          .id(it.id)
+          .addTo(this)
+    }
+  }
+}
+
+class UserModel(
+    private val user: User
+) : EpoxyModelWithHolder<UserModel.Holder>() {
+
+  override fun createNewHolder(): Holder = Holder()
+
+  override fun getDefaultLayout(): Int = R.layout.home_user_view
+
+  override fun bind(holder: Holder) {
+    holder.nameView.text = user.name
+    holder.screenNameView.text = user.screenName
+  }
+
+  class Holder : EpoxyHolder() {
+    lateinit var avatarView: ImageView
+    lateinit var nameView: TextView
+    lateinit var screenNameView: TextView
+
+    override fun bindView(itemView: View) {
+      //TODO: use ButterKnife or something like it
+      avatarView = itemView.findViewById(R.id.user_avatar) as ImageView
+      nameView = itemView.findViewById(R.id.user_name) as TextView
+      screenNameView = itemView.findViewById(R.id.user_screen_name) as TextView
+    }
+  }
+
+}
