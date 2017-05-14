5740085debc9d13d703b06f6adddd181124623b5
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-14 09:42:38 +0900

    Show user's avatar image

--- app/src/main/java/com/htoyama/licol/ui/home/user/list/UserController.kt
+++ app/src/main/java/com/htoyama/licol/ui/home/user/list/UserController.kt
@@ -1,5 +1,6 @@
 package com.htoyama.licol.ui.home.user.list
 
+import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,6 +9,7 @@ import com.airbnb.epoxy.EpoxyHolder
 import com.airbnb.epoxy.EpoxyModelWithHolder
 import com.htoyama.licol.R
 import com.htoyama.licol.domain.user.User
+import com.squareup.picasso.Picasso
 import javax.inject.Inject
 
 /**
@@ -40,17 +42,24 @@ class UserModel(
 
   override fun getDefaultLayout(): Int = R.layout.home_user_list_item
 
-  override fun bind(holder: Holder) {
-    holder.nameView.text = user.name
-    holder.screenNameView.text = user.screenName
+  override fun bind(h: Holder) {
+    //TODO: use Glide
+    Picasso.with(h.context)
+        .load(user.avatorUrl)
+        .into(h.avatarView)
+
+    h.nameView.text = user.name
+    h.screenNameView.text = user.screenName
   }
 
   class Holder : EpoxyHolder() {
+    lateinit var context: Context
     lateinit var avatarView: ImageView
     lateinit var nameView: TextView
     lateinit var screenNameView: TextView
 
     override fun bindView(itemView: View) {
+      context = itemView.context
       //TODO: use ButterKnife or something like it
       avatarView = itemView.findViewById(R.id.user_avatar) as ImageView
       nameView = itemView.findViewById(R.id.user_name) as TextView
