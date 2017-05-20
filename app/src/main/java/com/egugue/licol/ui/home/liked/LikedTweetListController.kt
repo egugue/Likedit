0fcbd20c3941b96b858627fc19831fbff7a17738
Haruyoshi Toyama <duane0728@gmail.com>
2017-05-20 18:24:37 +0900

    Show liked tweet

---
+++ app/src/main/java/com/egugue/licol/ui/home/liked/LikedTweetListController.kt
@@ -0,0 +1,41 @@
+package com.egugue.licol.ui.home.liked
+
+import com.airbnb.epoxy.EpoxyController
+import com.egugue.licol.application.likedtweet.LikedTweetPayload
+import com.egugue.licol.domain.likedtweet.LikedTweet
+import com.egugue.licol.domain.user.User
+import com.egugue.licol.ui.home.user.list.ProgressModel
+import javax.inject.Inject
+
+/**
+ * An [EpoxyController] which handles [LikedTweetPayload]
+ */
+class LikedTweetListController @Inject constructor() : EpoxyController()  {
+
+  private val progressModel = ProgressModel()
+
+  private var tweetUserList: List<LikedTweetPayload> = emptyList()
+  private var requireLoadingMore: Boolean = true
+
+  fun addData(l: List<LikedTweetPayload>) {
+    tweetUserList = ArrayList(tweetUserList + l)
+    requestModelBuild()
+  }
+
+  fun setLoadingMoreVisibility(requireLoadingMore: Boolean) {
+    this.requireLoadingMore = requireLoadingMore
+    requestModelBuild()
+  }
+
+  override fun buildModels() {
+    tweetUserList.forEach { (likedTweet, user) ->
+      LikedTweetModel(likedTweet, user)
+          .id(likedTweet.id)
+          .addTo(this)
+    }
+
+    progressModel
+        .id(-1)
+        .addIf(requireLoadingMore, this)
+  }
+}
