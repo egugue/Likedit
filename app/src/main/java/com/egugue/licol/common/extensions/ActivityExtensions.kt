95da2af6724c3a6c9502ad4c2987e2c2ccb04c96
Haruyoshi Toyama <duane0728@gmail.com>
2017-06-07 23:58:08 +0900

    Add custom tab as poorly-made

--- app/src/main/java/com/egugue/licol/common/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/egugue/licol/common/extensions/ActivityExtensions.kt
@@ -1,9 +1,15 @@
 package com.egugue.licol.common.extensions
 
 import android.app.Activity
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 
 
 fun Activity.toast(message: String)
     = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+@ColorInt
+fun Activity.color(@ColorRes res: Int): Int = ContextCompat.getColor(this, res)
 
--- app/src/main/java/com/egugue/licol/ui/usertweet/UserTweetActivity.kt
+++ app/src/main/java/com/egugue/licol/ui/usertweet/UserTweetActivity.kt
@@ -2,7 +2,9 @@ package com.egugue.licol.ui.usertweet
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import butterknife.bindView
@@ -15,6 +17,7 @@ import com.egugue.licol.domain.tweet.media.Photo
 import com.egugue.licol.domain.user.User
 import com.egugue.licol.ui.common.StateLayout
 import com.egugue.licol.ui.common.activity.BaseActivity
+import com.egugue.licol.ui.common.customtabs.CustomTabActivityHelper
 import com.egugue.licol.ui.common.recyclerview.DividerItemDecoration
 import com.egugue.licol.ui.home.liked.LikedTweetListController
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
@@ -50,6 +53,7 @@ class UserTweetActivity : BaseActivity() {
 
   @Inject lateinit var likedTweetAppService: LikedTweetAppService
   @Inject lateinit var listController: LikedTweetListController
+  @Inject lateinit var customTabHelper: CustomTabActivityHelper
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -63,6 +67,7 @@ class UserTweetActivity : BaseActivity() {
     initToolbar()
     initListView()
     loadMoreLikedTweet()
+    customTabHelper.setUpCustomTabService(this)
   }
 
   private fun initToolbar() {
@@ -75,13 +80,20 @@ class UserTweetActivity : BaseActivity() {
     recyclerView.addItemDecoration(DividerItemDecoration(this))
     recyclerView.addOnLoadMoreListener(object : LoadMoreListener {
       override fun onLoadMore() = loadMoreLikedTweet()
-      override fun isLoading(): Boolean = isLoading
-      override fun hasLoadedItems(): Boolean = hasLoadedItems
+      override fun isLoading() = isLoading
+      override fun hasLoadedItems() = hasLoadedItems
     })
 
     listController.callbacks = object : LikedTweetListController.AdapterCallbacks {
       override fun onTweetLinkClicked(url: String) {
         toast("url clicked $url")
+
+        val customTabIntent = CustomTabsIntent.Builder(customTabHelper.session)
+            .setToolbarColor(color(R.color.colorPrimary))
+            .setSecondaryToolbarColor(color(R.color.colorPrimaryDark))
+            .addDefaultShareMenuItem()
+           .build()
+        CustomTabActivityHelper.openCustomTab(this@UserTweetActivity,  customTabIntent, Uri.parse(url), null)
       }
 
       override fun onWholeTweetClicked(likedTweet: LikedTweet, user: User) {
